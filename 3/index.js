008ffd7e62113accbb07bd950ea5174861ab1f2f
CHODANIEL <eksldpf2299@naver.com>
2025-08-05 11:25:51 +0900

    3
    3연습

---
+++ 3/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+const userRouter =require("./routes/user")
+
+app.use(express.json())
+app.use("/users",userRouter)
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Express!")
+})
+app.listen(PORT, () => {
+    console.log("Server is running!")
+})
